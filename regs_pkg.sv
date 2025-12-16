176b7f1c7cf4a3716eada0965a924bb89135ae8b
Koutselakis Manos <koutselakismanos@gmail.com>
2025-12-16 12:15:41 +0200

    regs_pkg.sv

---
+++ regs_pkg.sv
@@ -0,0 +1,10 @@
+package regs_pkg;
+  localparam logic [7:0] RegSysId0 = 8'h00;
+  localparam logic [7:0] RegSysId1 = 8'h01;
+  localparam logic [7:0] RegSysId2 = 8'h02;
+  localparam logic [7:0] RegSysId3 = 8'h03;
+  localparam logic [7:0] RegSysId4 = 8'h04;
+  localparam logic [7:0] RegSysVersion = 8'h05;
+endpackage
+
+
